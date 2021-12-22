4d3d76fc80237b36d1cb30e05f2162f11273eb8a
Ali Ramazon <mr.safarali@gmail.com>
2021-12-21 21:59:08 -0700

    Check if two strings are anagrams

---
+++ 01_arraysAndStrings/02_isAnagrams.js
@@ -0,0 +1,25 @@
+/* 
+Given two strings. Check if one is permutation of another one. 
+*/
+
+const isAnagrams = (str1, str2) => {
+    if (str1.length !== str2.length) return false;
+    const hash = {};
+
+    for (let char of str1) {
+        if (hash[char]) {
+            hash[char]++;
+        } else {
+            hash[char] = 1;
+        }
+    }
+
+    for (let char of str2) {
+        if (!hash[char]) {
+            return false;
+        } else {
+            hash[char]--;
+        }
+    }
+    return true;
+};
